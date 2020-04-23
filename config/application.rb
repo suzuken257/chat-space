00d1bd5e032feabd5c9a3f312747becbfee18a62
suzuken257 <60838938+suzuken257@users.noreply.github.com>
2020-04-23 23:20:39 +0900

    push

--- config/application.rb
+++ config/application.rb
@@ -8,11 +8,12 @@ Bundler.require(*Rails.groups)
 
 module ChatSpace
   class Application < Rails::Application
-    config.generators do |g|
+      config.i18n.default_locale = :ja
       g.stylesheets false
       g.javascripts false
       g.helper false
       g.test_framework false
+      config.generators do |g|
     end
   end
 end
