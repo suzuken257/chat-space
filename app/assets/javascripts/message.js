155d58f44289c659ef9809fb938d634e0cc298cb
suzuken257 <60838938+suzuken257@users.noreply.github.com>
2020-04-29 12:18:23 +0900

    save

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,7 @@ $(function () {
   function buildHTML(message) {
     if (message.image) {
       var html = 
-        `<div class="main-list">
+        `<div class="main-list" data-message-id=${message.id} >
           <div class="main_content">
             <div class="main_content__user_name">
               ${message.user_name}
@@ -19,7 +19,7 @@ $(function () {
       return html;
     } else {
       var html =
-        `<div class="main-list">
+        `<div class="main-list" data-message-id=${message.id} >
           <div class="main_content">
             <div class="main_content__user_name">
               ${message.user_name}
@@ -50,6 +50,7 @@ $(function () {
     .done(function(data){
       var html = buildHTML(data);
       $('.messages').append(html);
+      console.log($('.messages').get())
       $('form')[0].reset();
       $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
       $('input').prop('disabled', false)
@@ -57,6 +58,33 @@ $(function () {
     .fail(function(){
       alert('error')
       $('input').prop('disabled', false);
-  })
+    })
   });
+
+  var reloadMessages = function() {
+    var last_message_id = $('.main-list:last').data("message-id");
+    $.ajax({
+      url:  "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+        });
+        $('.messages').append(insertHTML);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
